0067921d3bf67f387bdbf6cf4c8e801038348694
OnigiriJack <jackmihalik@icloud.com>
2020-11-17 16:32:32 +0900

    attempted to save succesful urls to a slice but not working currently

--- main.go
+++ main.go
@@ -2,57 +2,79 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func fetchUrl(url string, chFailedUrls chan string, chIsFinished chan bool){
+//chGoodUrls chan string,
+func fetchUrl(url string, chFailedUrls chan string, chIsFinished chan bool) {
 	client := http.Client{}
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("If-None-Match", `W/"wyzzy"`)
-	resp, err := client.Do(req) //sends a request and gets a response
-
+	resp, err := client.Do(req)
+	//sends a request and gets a response
+	// executed when fetch url outer scope is finished
 	defer func() {
 		chIsFinished <- true
 	}()
 
-	if err!= nil || resp.StatusCode != 200 {
-	 chFailedUrls <- url
-	 return
- }
+	if err != nil || resp.StatusCode != 200 {
+		chFailedUrls <- url
+		return
+	}
+
+	// 	fmt.Println("here")
+	// 	chGoodUrls <- url
+
+	//}
 }
 
+func getHtmlFromPage(url string) {
+	resp, _ := http.Get(url)
+	bytes, _ := ioutil.ReadAll(resp.Body)
 
+	fmt.Println("HTML:\n\n", string(bytes), bytes)
+	resp.Body.close()
+}
 func main() {
 
 	urlsList := [10]string{
-			 "http://example1.com",
-			 "http://example2.com",
-			 "http://example3.com",
-			 "http://example4.com",
-			 "http://example5.com",
-			 "http://example10.com",
-			 "http://example20.com",
-			 "http://example30.com",
-			 "http://example40.com",
-			 "http://example50.com",
-	 }
+		"https://natgeo.nikkeibp.co.jp",
+		"http://example2.com",
+		"http://example3.com",
+		"http://example4.com",
+		// "http://example5.com",
+		// "http://example10.com",
+		// "http://example20.com",
+		// "http://example30.com",
+		"http://example40.com",
+		// "http://example50.com",
+	}
 
-	 chFailedUrls := make(chan string)
-	 chIsFinished:= make(chan bool)
+	chFailedUrls := make(chan string)
+	//chGoodUrls := make(chan string)
+	chIsFinished := make(chan bool)
+	// do each fetch in the list concurrently
+	for _, url := range urlsList {
+		fmt.Println(url)
+		go fetchUrl(url, chFailedUrls, chIsFinished)
+	}
 
-	 for _, url := range urlsList {
-		 go fetchUrl(url, chFailedUrls, chIsFinished)
-	 }
+	failedUrls := make([]string, 0)
+	//goodUrls := make([]string, 0)
 
-	 failedUrls := make([]string, 0)
-	 for i := 0 ; i < len(urlsList); {
-		 select {
-		 case url := <-chFailedUrls:
-			 failedUrls = append(failedUrls, url)
-		 case <- chIsFinished:
-			 i++
-		 }
-	 }
-	 fmt.Println("could not fetch these urls: ", failedUrls)
+	for i := 0; i < len(urlsList); {
+		select {
+		case url := <-chFailedUrls:
+			failedUrls = append(failedUrls, url)
+		//case url := <-chGoodUrls:
+		//	goodUrls = append(goodUrls, url)
+		case <-chIsFinished:
+			i++
+		}
+
+	}
+	fmt.Println("could not fetch these urls: ", failedUrls)
+	fmt.Println("could fetch these urls: ", failedUrls)
 
 }
